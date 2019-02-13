7f2dce15adc43566fb3efc203a810e29777057b1
Joel Austin <jta26@Pitt.edu>
2019-02-13 11:41:22 -0500

    simple https using GreenLock

--- API/index.js
+++ API/index.js
@@ -1,9 +1,24 @@
+var fs = require('fs');
 var express = require('express');
+var greenlock = require('greenlock-express');
 var app = express();
-var router = require('./routes/router')
-const port = 3000;
-
-
+var router = require('./routes/router');
 
 app.use('/', router)
-app.listen(port, () => console.log(`App listening on ${port}`))
+
+
+greenlock.create({
+    version: 'draft-11',
+    server: 'https://acme-v02.api.letsencrypt.org/directory',
+
+    email:'jta26@Pitt.edu',
+    approveDomains: ['joelaustin.net', 'www.joelaustin.net'],
+    agreeTos: true,
+    configDir: './',
+
+    app: require('./index.js'),
+
+    communityMember: true,
+    telemetry: true
+}).listen(80, 443);
+
