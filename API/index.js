e9dcd5e7a69104c3aef5e491062c59a7a8fcadf2
Joel Austin <jta26@Pitt.edu>
2019-02-14 02:02:24 -0500

    Final Cert Code

--- API/index.js
+++ API/index.js
@@ -1,40 +1,17 @@
 var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var morgan = require('morgan');
-
-var http = require('http');
-var https = require('https');
-var privatekey = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net/privkey.pem');
-var cert = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net/fullchain.pem');
-
-var creds = {key: privatekey, cert: cert};
-
-var greenlock = require('greenlock-express');
+var helmet = require('helmet');
 var app = express();
 var router = require('./routes/router');
 
+var privatekey = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net-0001/privkey.pem');
+var cert = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net-0001/fullchain.pem');
+var creds = {key: privatekey, cert: cert};
+
+app.use(helmet());
 app.use(morgan('tiny'));
-app.use(express.static('public'));
 app.use('/', router)
 
-var httpServer = http.createServer(app);
-var httpServer = https.createServer(creds, app);
-
-httpServer.listen(80);
-httpServer.listen(443);
-// greenlock.create({
-//     version: 'draft-11',
-//     server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
-
-//     email:'jta26@Pitt.edu',
-//     approveDomains: ['api.joelaustin.net'],
-//     agreeTos: true,
-//     configDir: './',
-
-//     app: function(req, res) {
-//         require('./index.js');
-//     },
-
-//     communityMember: true,
-//     telemetry: true
-// }).listen(80, 443);
+https.createServer(creds, app).listen(443);
