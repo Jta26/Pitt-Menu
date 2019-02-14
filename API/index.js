8b6b585d3dcd536420be7215a3b6fc190f9152e6
Joel Austin <jta26@Pitt.edu>
2019-02-13 23:42:51 -0500

    Cert Changes

--- API/index.js
+++ API/index.js
@@ -1,28 +1,40 @@
 var fs = require('fs');
 var express = require('express');
 var morgan = require('morgan');
+
+var http = require('http');
+var https = require('https');
+var privatekey = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net/privkey.pem');
+var cert = fs.readFileSync('/etc/letsencrypt/live/api.joelaustin.net/fullchain.pem');
+
+var creds = {key: privatekey, cert: cert};
+
 var greenlock = require('greenlock-express');
 var app = express();
 var router = require('./routes/router');
 
 app.use(morgan('tiny'));
-
+app.use(express.static('public'));
 app.use('/', router)
 
+var httpServer = http.createServer(app);
+var httpServer = https.createServer(creds, app);
 
-greenlock.create({
-    version: 'draft-11',
-    server: 'https://acme-v02.api.letsencrypt.org/directory',
+httpServer.listen(80);
+httpServer.listen(443);
+// greenlock.create({
+//     version: 'draft-11',
+//     server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
 
-    email:'jta26@Pitt.edu',
-    approveDomains: ['api.joelaustin.net'],
-    agreeTos: true,
-    configDir: './',
+//     email:'jta26@Pitt.edu',
+//     approveDomains: ['api.joelaustin.net'],
+//     agreeTos: true,
+//     configDir: './',
 
-    app: function(req, res) {
-        require('./index.js');
-    },
+//     app: function(req, res) {
+//         require('./index.js');
+//     },
 
-    communityMember: true,
-    telemetry: true
-}).listen(80, 443);
+//     communityMember: true,
+//     telemetry: true
+// }).listen(80, 443);
--- API/services/google.js
+++ API/services/google.js
@@ -17,12 +17,8 @@ function getMenu(agent) {
     var menutype = agent.parameters.menu;
     var date = agent.parameters.date;
     var timeperiod = agent.parameters['time-period'];
-
+    
 }
-
-
-
-
 function googleWebhookProcessor(req, res) {
     const agent = new WebhookClient({request: req, response: res});
     let intentMap = new Map();
