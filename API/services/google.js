363e30718c70c708276e5dc6de5e812dfb52da63
Joel Austin <jta26@Pitt.edu>
2019-02-14 23:39:14 -0500

    forEachAsync

--- API/services/google.js
+++ API/services/google.js
@@ -8,13 +8,13 @@ const {
   } = require('actions-on-google');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const sqlService = require('./sqlservice');
+const forEachAsync = require('forEachAsync');
 
 function welcomeIntent(agent) {
     var ssml = "<speak>Welcome to Pitt Menu! <break time='.3s'/> You can ask things such as, <break time='.3s'/> What's for lunch today? <break time='.3s'/> or <break time='.3s'/> What's for supper on thursday? </speak>"
     agent.add(ssml);
 }
-
-function getMenuIntent(agent) {
+function menuIntent(agent) {
     var menutype = agent.parameters.menu;
     var date = agent.parameters.date;
     var timeperiod = agent.parameters['time-period'];
@@ -27,20 +27,22 @@ function getMenuIntent(agent) {
         menutypeBool = 0;
     }
     console.log(menutype, menutypeBool, date, timeperiod);
+    return getMenu(menutypeBool, date).then((items) => {
+        agent.add(`<speak> For ${menutype} on ${date} is: ${items}</speak>`);
+    }).catch(() => {
+        
+    });
+}
+function getMenu(menutypeBool, date) {
     return new Promise((resolve, reject) => {
-        agent.add(`<speak> For ${menutype} on ${date} is:</speak>`);
         sqlService(date, menutypeBool, function(result) {
             items = '';
-            count = 0;
-            result[0].forEach((item, index, arr) => {
-                console.log(item['Item Name']);
-                agent.add('<speak> ' + item['Item Name'] + '</speak>');
-                count++; 
-                if (count === arr.length) {
-                    console.log(items);
-                    resolve();     
-                }
-            });
+            forEachAsync(result[0], (next, item, index, arr) => {
+                items.concat(item['Item Name'] + '<break time=".3s"/> ');
+            }).then(() => {
+                resolve(items);
+            })
+            
         });
         
     });
