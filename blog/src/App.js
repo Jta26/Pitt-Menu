2bb02efe570624df26621465d38445a76fa4c3b1
Joel Austin <joel.austin@ipc.com>
2019-03-04 22:41:59 -0500

    New Blog Update

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import './css/App.css';
+
+import 'semantic-ui-css/semantic.min.css'
+
+import TransitionShell from './components/TransitionShell';
+
+import Home from './pages/home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div>
+             <Switch>
+              <Route path='/' component={TransitionShell(Home)}/>
+            </Switch>
+          </div>
       </div>
     );
   }
---
+++ blog/src/pages/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../css/home.css';
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return(
+            <div>
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
