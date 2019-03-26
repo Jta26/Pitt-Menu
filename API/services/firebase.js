35758cf2a822803a5b114d4b34cbf670602d8b48
Jta26 <Jta26@Pitt.edu>
2019-03-26 17:21:54 -0400

    Updated Firebase Function;  Store Image function

--- API/services/firebase.js
+++ API/services/firebase.js
@@ -1,6 +1,8 @@
 var sql = require('./sqlservice');
 var fs = require('fs');
 var firebase = require('firebase');
+
+//DELETE EVENTUALLY
 var app = firebase.initializeApp({
     apiKey: "AIzaSyDpBoyI13RY9mHU2C1cOSyAC_tfr3-hu5U",
     authDomain: "menu-parser-7ba02.firebaseapp.com",
@@ -12,6 +14,7 @@ var app = firebase.initializeApp({
     storageBucket: "menu-parser-7ba02.appspot.com",
     messagingSenderId: "1027964664238"
   });
+
 var admin = require('firebase-admin');
 var serviceAccount = require('../menu-parser-service-account.json');
 admin.initializeApp({
@@ -20,7 +23,7 @@ admin.initializeApp({
     storageBucket: "menu-parser-7ba02.appspot.com"
 });
 console.log('firebase initialized');
-
+let database = admin.database();
 //This file handles functions that map 
 // tenency of the items in the MySQL database 
 // to the items in the firebase realtime database.
@@ -29,32 +32,41 @@ console.log('firebase initialized');
 //function that returns promise that gets the items from the database.
 function GetFirebaseItemList() {
     return new Promise((res, rej) => {
-        var database = firebase.database();
+      
         var itemsRef =  database.ref('/items/');
         var items = [];
         itemsRef.once('value').then((snapshot) => {
             snapshot.forEach((item) => {
-                items.push(item.val());
+                items.push([item.key, item.val()]);
+                
             });
             res(items);
     
         })
     })
-
 }
-function StoreImageFromUInt8Arr(imgName, imgBuffer) {
-    return new Promise( async (res, rej) => {
+function StoreImageFromUInt8Arr(imgName, itemID, imgBuffer) {
+    return new Promise(async (res, rej) => {
         var bucket = admin.storage().bucket();
         var file = bucket.file(imgName + '.jpg');
         file.save(imgBuffer);
-    })
+        var fileUrl = await file.getSignedUrl({
+            action: 'read',
+            expires: '03-17-2025'
+        });
+        console.log(`the Signed URL for ${imgName} is ${fileUrl[0]}`);
+        CreateDatabaseReferenceLink(itemID, fileUrl[0]);
+    });
+}
+function CreateDatabaseReferenceLink(itemID, link) {
+    let ref = database.ref(`items/${itemID}/images`);
+    ref.push(link);
+    console.log('pushed');
 }
 function updateFirebase() {
     sql.GetItems().then((items) => {
-        var database = firebase.database();
-       
         items.forEach(item => {
-            database.ref('/items/' + item.ItemID).set({
+            database.ref('/items/' + item.ItemID).update({
                 name: item.ItemName
             });
         });
